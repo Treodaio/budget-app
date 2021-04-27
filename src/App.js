70c819e1333bde1df50df25ce469756e6c0988db
Slawomir Pietrzak <1pietrzakslawomir@gmail.com>
2021-04-27 13:29:45 +0200

    add absolute imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from 'logo.svg';
+import 'App.css';
 
 // odnosimy się do index.js z components który eksportuje naviagation
-import { Navigation } from './components';
+import { Navigation } from 'components';
 
 function App() {
 
